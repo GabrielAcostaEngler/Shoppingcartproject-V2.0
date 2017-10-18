09db859370bda6cbe98e5ba3672b88c80913751c
Gabriel <gabi.acosta.engler@gmail.com>
2017-10-19 00:19:11 +0200

    Created a controller for for authorizeTransaction.

--- Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/controllers/RequestController.java
+++ Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/controllers/RequestController.java
@@ -11,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ShoppingCart.Project.springbootshoppingcart.AuthorizeTransactionInput;
 import com.ShoppingCart.Project.springbootshoppingcart.VerifyUserInput;
 import com.ShoppingCart.Project.springbootshoppingcart.httpClientService.RequestHandler;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 
 
 @Controller
@@ -28,13 +31,13 @@ public class RequestController {
 	public void verifyUser(@RequestBody String indata, HttpServletRequest request, HttpServletResponse response) {
 		
 		try {
-		VerifyUserInput verifyUserInput = new VerifyUserInput(indata);
-		String verifyUserResponse = rh.verifyUserHandler(verifyUserInput);
+			VerifyUserInput verifyUserInput = new VerifyUserInput(indata);
+			String verifyUserResponse = rh.verifyUserHandler(verifyUserInput);
+				
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().write(verifyUserResponse);
 			
-		response.setStatus(HttpServletResponse.SC_OK);
-		response.getWriter().write(verifyUserResponse);
-		
-		return;
+			return;
 		
 		} catch (Exception e) {
 		response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
@@ -42,6 +45,28 @@ public class RequestController {
 		return;
 		}
 		
+	}
+	
+	
+	@RequestMapping(value="/authorize", method = RequestMethod.POST)
+	@ResponseBody
+	public void authorizeUser(@RequestBody String indata, HttpServletRequest request, HttpServletResponse response) {
+		
+		try {
+			AuthorizeTransactionInput authorizeTransactionInput = new AuthorizeTransactionInput(indata);
+			String authorizeTransactionResponse = rh.authorizeTransactionHandler(authorizeTransactionInput);
+			
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().write(authorizeTransactionResponse);
+			
+			
+			return;
+			
+			
+		} catch(Exception e){
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return;
+		}
 	}
 	
 	
