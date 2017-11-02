0ea0fe1b5ac0b726e09d9bf19f920fb54a96a102
Gabriel <gabi.acosta.engler@gmail.com>
2017-11-02 13:56:26 +0100

    Created a mini functional Frontend for neteller payment.(Still in progress) gave the result templte some bootstrap.

--- Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/shoppingcartappcontroller/ViewController.java
+++ Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/shoppingcartappcontroller/ViewController.java
@@ -1,13 +1,23 @@
 package com.ShoppingCart.Project.springbootshoppingcart.shoppingcartappcontroller;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 import com.ShoppingCart.Project.springbootshoppingcart.NetellerTxInput;
+import com.google.gson.Gson;
 
 @Controller
 public class ViewController {
@@ -22,13 +32,50 @@ public class ViewController {
 
 	@PostMapping("/submitinput")
 	public String informationInput(@ModelAttribute NetellerTxInput netellerTxInput) {
-		
 
-		netellerTxInput.setUserId("1");
-		netellerTxInput.setSessionId("2");
-		netellerTxInput.setMerchantId("1992");
-		
+		String piqUrl = "https://test-api.paymentiq.io/paymentiq/api/neteller/deposit/process";
+
+		try {
+			CloseableHttpClient client = HttpClientBuilder.create().build();
+			HttpPost postRequest = new HttpPost(piqUrl);
+			postRequest.setHeader("accept", "application/json");
+			postRequest.setHeader("content-type", "application/json");
+			
+			netellerTxInput.setUserId("1");
+			netellerTxInput.setSessionId("2");
+			netellerTxInput.setMerchantId("1992");
+			
+			String payLoad = new Gson().toJson(netellerTxInput);
+			StringEntity reqEntity = new StringEntity(payLoad,"UTF-8");
+
+			reqEntity.setChunked(false);
+	        
+			postRequest.setEntity(reqEntity);
+			postRequest.setHeader("accept", "application/json");
+			postRequest.setHeader("content-type", "application/json");
+
+			HttpResponse response = client.execute(postRequest);
+			HttpEntity respEntity = response.getEntity();
+			
+			String content = EntityUtils.toString(respEntity);
+			System.out.println(content);
+			
+			
+
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
+				
+			}
+			
+
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		return "result";
+
 	}
 
 }
