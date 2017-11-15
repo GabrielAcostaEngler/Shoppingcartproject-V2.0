4086becf3aaee5910300be4285fc822db6caa87a
Gabriel <gabi.acosta.engler@gmail.com>
2017-11-15 16:56:27 +0100

    added spring security for authentication.

---
+++ Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/security/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.ShoppingCart.Project.springbootshoppingcart.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		//@formatter:off
+		
+		http
+			.authorizeRequests()
+				.antMatchers(
+						"/")
+				.permitAll()
+				.antMatchers(
+					"/css/*",
+					"/img/*").permitAll()
+			.anyRequest()
+				.authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.usernameParameter("email")
+				.passwordParameter("password")
+				.defaultSuccessUrl("/")
+				.permitAll();
+		
+		//@formatter:on
+
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
+		//@formatter:off
+		auth.inMemoryAuthentication()
+		.withUser("username")
+		.password("password")
+		.roles("USER");
+		
+		//@formatter:on
+	}
+
+}
