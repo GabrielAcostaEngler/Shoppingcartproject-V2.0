5a5c1ba248821f0a2b779bc307c5594e408356fd
Gabriel <gabi.acosta.engler@gmail.com>
2017-11-15 16:57:06 +0100

    created AuthController class with a login controller inside.

---
+++ Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/shoppingcartappcontroller/AuthController.java
@@ -0,0 +1,17 @@
+package com.ShoppingCart.Project.springbootshoppingcart.shoppingcartappcontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AuthController {
+
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	String admin() {
+		
+		return "loginpage";
+	}
+
+}
