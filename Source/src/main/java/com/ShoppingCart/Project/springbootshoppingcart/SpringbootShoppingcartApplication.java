1e21859cd2bf7bf0c48e6be078bcbe50e257ddfa
Gabriel <gabi.acosta.engler@gmail.com>
2017-11-22 21:31:05 +0100

    Put a commandLinaRunner for adding a user to the in-memory database everytime at startup(for testing)

--- Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/SpringbootShoppingcartApplication.java
+++ Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/SpringbootShoppingcartApplication.java
@@ -1,11 +1,15 @@
 package com.ShoppingCart.Project.springbootshoppingcart;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class SpringbootShoppingcartApplication extends SpringBootServletInitializer {
 
 	@Override
@@ -15,6 +19,23 @@ public class SpringbootShoppingcartApplication extends SpringBootServletInitiali
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootShoppingcartApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner runner(UserDao user) {
+		
+		return args -> {
+			user.save(new SiteUser("Gabriel",
+									"Acosta",
+									"stnbrohultsvgn",
+									"Uppsala",
+									"75758",
+									"Sweden",
+									"ga_engler@hotmail.com",
+									"hejhej",
+									"1992-10-06",
+									"0767105222"));
+		};
 	}
 
 }
