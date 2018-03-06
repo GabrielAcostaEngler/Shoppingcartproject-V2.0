08c29df9c38abd55e9d986614a5447edd291b65d
Gabriel <gabi.acosta.engler@gmail.com>
2018-03-06 06:13:05 +0100

    Created PiqTx for saving Tx data

---
+++ Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/PiqTx.java
@@ -0,0 +1,15 @@
+package com.ShoppingCart.Project.springbootshoppingcart;
+
+import com.ShoppingCart.Project.springbootshoppingcart.piqcallbackhandler.callbackinput.*;
+
+public class PiqTx {
+	
+	private VerifyUserInput verifyUserData;
+	
+	private AuthorizeTxInput authorizeTxData;
+	
+	private CancelTxInput cancelTxData;
+	
+	private TransferTxInput transferTxData;
+
+}
