59fe7b36f3d6e762bf8d35fc6d12dfc415a9f443
DESKTOP-993H2D0\Ga_En <ga_engler@hotmail.com>
2018-03-12 16:02:29 +0100

    changed name from usrdao to userDao.

--- Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/UserService.java
+++ Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/UserService.java
@@ -16,19 +16,19 @@ import org.springframework.stereotype.Service;
 public class UserService implements UserDetailsService {
 
 	@Autowired
-	private UserDao usrDao;
+	private UserDao userDao;
 	
 	
 
 	public void register(SiteUser user) {
-		usrDao.save(user);
+		userDao.save(user);
 	}
 
 	public SiteUser getCurrentSiteUser() {
 
 		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		String name = user.getUsername();
-		SiteUser siteUser = usrDao.findByEmail(name);
+		SiteUser siteUser = userDao.findByEmail(name);
 
 		return siteUser;
 	}
@@ -36,7 +36,7 @@ public class UserService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-		SiteUser user = usrDao.findByEmail(email);
+		SiteUser user = userDao.findByEmail(email);
 
 		if (user == null) {
 
