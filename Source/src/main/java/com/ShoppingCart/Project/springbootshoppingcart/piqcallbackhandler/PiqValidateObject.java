83ab1bacfb051305c0b77d4dc735401f61f38dad
DESKTOP-993H2D0\Ga_En <ga_engler@hotmail.com>
2018-03-12 16:07:44 +0100

    added two message types and made attributes private to class.

--- Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/piqcallbackhandler/PiqValidateObject.java
+++ Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/piqcallbackhandler/PiqValidateObject.java
@@ -3,10 +3,12 @@ package com.ShoppingCart.Project.springbootshoppingcart.piqcallbackhandler;
 public class PiqValidateObject {
 	
 	
-	boolean success;
-	String resultMessage = "";
+	private boolean success;
+	private String resultMessage = "";
 	
 	final String ERROR_USERID_DOES_NOT_MATCH = "ERROR_USERID_DOES_NOT_MATCH";
+	final String ERROR_NOT_ENOUGH_CURRENCY = "ERROR_NOT_ENOUGH_CURRENCY";
+	final String ERROR_TXCY_DOES_NOT_MATCH_BALANCECY = "ERROR_TXCY_DOES_NOT_MATCH_BALANCECY";
 
 
 	public PiqValidateObject() {
