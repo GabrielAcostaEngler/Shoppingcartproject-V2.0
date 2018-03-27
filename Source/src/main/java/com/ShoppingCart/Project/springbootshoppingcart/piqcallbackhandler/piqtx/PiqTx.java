ff4e5ecf290d90fb9ae511ac5f6c6404f6714329
DESKTOP-993H2D0\Ga_En <ga_engler@hotmail.com>
2018-03-27 15:50:40 +0200

    Fixed imports and added txSuccess and txMsg attributes for displaying if a tx is successfull and add a msg if it isn't.

--- Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/piqcallbackhandler/piqtx/PiqTx.java
+++ Source/src/main/java/com/ShoppingCart/Project/springbootshoppingcart/piqcallbackhandler/piqtx/PiqTx.java
@@ -1,4 +1,4 @@
-package com.ShoppingCart.Project.springbootshoppingcart.piqcallbackhandler;
+package com.ShoppingCart.Project.springbootshoppingcart.piqcallbackhandler.piqtx;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -31,55 +31,81 @@ public class PiqTx {
 	private String authorizeTxResponse;
 	private String transferTxResponse;
 	private String cancelTxResponse;
+	private String txSuccess;
+	private String txMsg;
 
 	
 	
 
 	//getters
+	
+	
 	public Long getTxId() {
 		return txId;
 	}
+	
 	public String getUserId() {
 		return userId;
 	}
+	
 	public String getSessionId() {
 		return sessionId;
 	}
+	
 	public Double getTxAmount() {
 		return txAmount;
 	}
+	
 	public String getTxAmountCy() {
 		return txAmountCy;
 	}
+	
 	public String getPiqTxId() {
 		return piqTxId;
 	}
+	
 	public int getTxTypeId() {
 		return txTypeId;
 	}
+	
 	public String getTxName() {
 		return txName;
 	}
+	
 	public String getProvider() {
 		return provider;
 	}
+	
 	public String getAuthCode() {
 		return authCode;
 	}
+	
 	public String getTxPspAmount() {
 		return txPspAmount;
 	}
+	
 	public String getTxPspAmountCy() {
 		return txPspAmountCy;
 	}
+	
 	public String getFee() {
 		return fee;
 	}
+	
 	public String getFeeCy() {
 		return feeCy;
 	}
+	
 	public String getTxRefId() {
 		return txRefId;
+	}
+	
+	public String getTxSuccess() {
+		return txSuccess;
+	}
+	
+	public String getTxMsg() {
+		return txMsg;
 	}
 	
 	public String getVerifyUserResponse() {
@@ -104,48 +130,63 @@ public class PiqTx {
 	public void setTxId(Long txId) {
 		this.txId = txId;
 	}
+	
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+	
 	public void setSessionId(String sessionId) {
 		this.sessionId = sessionId;
 	}
+	
 	public void setTxAmount(Double txAmount) {
 		this.txAmount = txAmount;
 	}
+	
 	public void setTxAmountCy(String txAmountCy) {
 		this.txAmountCy = txAmountCy;
 	}
+	
 	public void setPiqTxId(String piqTxId) {
 		this.piqTxId = piqTxId;
 	}
+	
 	public void setTxTypeId(int txTypeId) {
 		this.txTypeId = txTypeId;
 	}
+	
 	public void setTxName(String txName) {
 		this.txName = txName;
 	}
+	
 	public void setProvider(String provider) {
 		this.provider = provider;
 	}
+	
 	public void setAuthCode(String authCode) {
 		this.authCode = authCode;
 	}
+	
 	public void setTxPspAmount(String txPspAmount) {
 		this.txPspAmount = txPspAmount;
 	}
+	
 	public void setTxPspAmountCy(String txPspAmountCy) {
 		this.txPspAmountCy = txPspAmountCy;
 	}
+	
 	public void setFee(String fee) {
 		this.fee = fee;
 	}
+	
 	public void setFeeCy(String feeCy) {
 		this.feeCy = feeCy;
 	}
+	
 	public void setTxRefId(String txRefId) {
 		this.txRefId = txRefId;
 	}
+	
 	public void setVerifyUserResponse(String verifyUserResponse) {
 		this.verifyUserResponse = verifyUserResponse;
 	}
@@ -161,6 +202,16 @@ public class PiqTx {
 	public void setCancelTxResponse(String cancelTxResponse) {
 		this.cancelTxResponse = cancelTxResponse;
 	}
+
+	public void setTxSuccess(String success) {
+		this.txSuccess = success;
+	}
+
+	public void setTxMsg(String txMsg) {
+		this.txMsg = txMsg;
+	}
+	
+	
 
 	
 
